76586daeb348ae5d05728ec52708628aac877dec
Galen Sho Sato <galen.sato@gmail.com>
2023-01-17 17:52:49 -0500

    Fixed getRandomSightings()

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 // This is just allowing us to use the dataset from the other file. You can ignore this line.
 const UFO_SIGHTINGS = require("./nyc-ufo-sightings.json");
+
+
+function calculateMinutes(seconds) {
+    let x = parseInt(seconds)
+    return minutes = Math.floor( x / 60)
+}
+
+function getRandomSighting(array) {
+    return array[Math.floor(Math.random() * array.length)]
+}
+
+function getRandomSightings(number) {
+    let x = []
+    for (i = 0; i < number; i++) {
+        let y = getRandomSighting(UFO_SIGHTINGS)
+        x.push(y)
+    }
+    return x
+
+}
+
+
+
+console.log(getRandomSightings(2))
